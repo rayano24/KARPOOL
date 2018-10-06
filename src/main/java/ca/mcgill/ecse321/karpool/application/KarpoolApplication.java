b3144e267774ae95a67350c5582705497b74a56b
larakollokian <lara.kollokian@gmail.com>
2018-10-05 21:36:01 -0400

    first commit attempt 5

---
+++ src/main/java/ca/mcgill/ecse321/karpool/application/KarpoolApplication.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.karpool.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarpoolApplication.class, args);
+	}
+}
