f0d301f1619955428ff1dd1c2d6f9af048c41e68
Ammar Rudani <aarudani7@gmail.com>
2018-10-06 17:41:19 -0400

    Repository class
    created repository class

--- src/main/java/ca/mcgill/ecse321/karpool/application/Driver.java
+++ src/main/java/ca/mcgill/ecse321/karpool/application/Driver.java
@@ -1,34 +1,41 @@
 package ca.mcgill.ecse321.karpool.application;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import ca.mcgill.ecse321.karpool.model.UserRole;
 import ca.mcgill.ecse321.karpool.model.Car;
-import javax.persistence.OneToOne;
 import ca.mcgill.ecse321.karpool.model.Trip;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Driver extends UserRole{
    private Car car;
-   
-   @OneToOne(optional=false)
-   public Car getCar() {
-      return this.car;
-   }
-   
-   public void setCar(Car car) {
-      this.car = car;
-   }
-   
-   private Trip trip;
-   
-   @ManyToOne(optional=false)
-   public Trip getTrip() {
-      return this.trip;
-   }
-   
-   public void setTrip(Trip trip) {
-      this.trip = trip;
-   }
-   
-   }
+
+public void setCar(Car value) {
+    this.car = value;
+}
+@Id
+public Car getCar() {
+    return this.car;
+}
+private Trip trip;
+
+public void setTrip(Trip value) {
+    this.trip = value;
+}
+
+@Id
+public Trip getTrip() {
+    return this.trip;
+}
+private int driverId;
+
+public void setDriverId(int value) {
+    this.driverId = value;
+}
+
+@Id
+public int getDriverId() {
+    return this.driverId;
+}
+}
--- src/main/java/ca/mcgill/ecse321/karpool/application/Passenger.java
+++ src/main/java/ca/mcgill/ecse321/karpool/application/Passenger.java
@@ -1,21 +1,29 @@
 package ca.mcgill.ecse321.karpool.application;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import ca.mcgill.ecse321.karpool.model.UserRole;
 import ca.mcgill.ecse321.karpool.model.Trip;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Passenger extends UserRole{
    private Trip trip;
-   
-   @ManyToOne(optional=false)
-   public Trip getTrip() {
-      return this.trip;
-   }
-   
-   public void setTrip(Trip trip) {
-      this.trip = trip;
-   }
-   
-   }
+
+public void setTrip(Trip value) {
+    this.trip = value;
+}
+@Id
+public Trip getTrip() {
+    return this.trip;
+}
+private int passengerId;
+
+public void setPassengerId(int value) {
+    this.passengerId = value;
+}
+@Id
+public int getPassengerId() {
+    return this.passengerId;
+}
+}
