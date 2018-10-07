e3feb2ce2d5d0ea983d1c250843bf445170b448d
Rayan Osseiran <10555572+rayano24@users.noreply.github.com>
2018-10-07 13:27:01 -0400

    Explicitly mentioning SpringBoot Class

--- src/test/java/ca/mcgill/ecse321/karpool/KarpoolApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/karpool/KarpoolApplicationTests.java
@@ -13,12 +13,14 @@ import org.mockito.invocation.InvocationOnMock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import ca.mcgill.ecse321.karpool.application.KarpoolApplication;
 import ca.mcgill.ecse321.karpool.application.controller.KarpoolController;
 import ca.mcgill.ecse321.karpool.application.repository.KarpoolRepository;
 import ca.mcgill.ecse321.karpool.model.User;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = KarpoolApplication.class)
 public class KarpoolApplicationTests
 {
 	@Mock
