6669cc50d4c2aeeee15d3835cbc90de5550eb291
Rayan Osseiran <10555572+rayano24@users.noreply.github.com>
2018-10-07 11:42:39 -0400

    More changes to controller methods

--- src/main/java/ca/mcgill/ecse321/karpool/application/controller/KarpoolController.java
+++ src/main/java/ca/mcgill/ecse321/karpool/application/controller/KarpoolController.java
@@ -19,15 +19,53 @@ public class KarpoolController {
 	@Autowired
 	KarpoolRepository repository;
 
+	//TODO: Password can't just be read as a regular String. NEEDS some form of encryption.
+	//FIXME: Why does createUser need to return the usersName?
+	
+	
+	/**
+	 * @param name
+	 * @param email
+	 * @param password
+	 * @param phone
+	 * @param rating
+	 * @return the users name
+	 */
 	@PostMapping("/users/{email}")
 	public String createUser(@PathVariable("name")String name, String email, String password, String phone, Rating rating)
 	{
 		User user = repository.createUser(name, email, password, phone, rating);
 		return user.getName();
 	}
-
+	
+	/**
+	 * 
+	 * @param email
+	 * @param password
+	 * @return OK if the account is authenticated 
+	 */
 	@GetMapping("/users/{email}")
-	public String queryUser(@PathVariable("name")String email)
+	public boolean authenticateUser(@PathVariable("email")String email, @PathVariable("password")String password)
+	{
+		try {
+			User user = repository.getUser(email);
+			if(user.getPassword().equals(password)) 
+				return true;	
+		}
+		catch(NullPointerException e) {
+			return false;
+		}
+		
+		return false;	
+	}
+	
+	/**
+	 * 
+	 * @param email
+	 * @return the account that was searched for 
+	 */
+	@GetMapping("/users/{email}")
+	public String queryUser(@PathVariable("email")String email)
 	{
 		User user = repository.getUser(email);
 		if(user == null)
@@ -37,10 +75,15 @@ public class KarpoolController {
 		return user.getName();
 	}
 	
+	
+	/**
+	 * Add a  rating to the user 
+	 * @param email
+	 * @param rating
+	 */
 	@GetMapping("/users/{email}")
 	public void addRating(@PathVariable("email")String email, Rating rating)
 	{
-		
 		try {
 			User user = repository.getUser(email);
 			user.setRating(rating);
