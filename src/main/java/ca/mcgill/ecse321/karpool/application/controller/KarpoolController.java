048217491992c1960b6601b6fd9a8169a2047f73
larakollokian <lara.kollokian@gmail.com>
2018-10-07 15:32:48 -0400

    re added imports in controller file

--- src/main/java/ca/mcgill/ecse321/karpool/application/controller/KarpoolController.java
+++ src/main/java/ca/mcgill/ecse321/karpool/application/controller/KarpoolController.java
@@ -7,22 +7,33 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ca.mcgill.ecse321.karpool.model.Trip;
+import ca.mcgill.ecse321.karpool.model.User;
+import ca.mcgill.ecse321.karpool.application.*;	
+import ca.mcgill.ecse321.karpool.application.repository.*;
+
 public class KarpoolController {
-	
+
 
 
 	public static final String ERROR_NOT_FOUND_MESSAGE = "Not found";
-	
+
 	@Autowired
 	KarpoolRepository repository;
-	
+
 	@Autowired
 	Driver driver;
 
 	//TODO: Password can't just be read as a regular String. NEEDS some form of encryption.
 	//FIXME: Why does createUser need to return the usersName?
-	
-	
+
+
 	/**
 	 * @param name
 	 * @param email
@@ -37,32 +48,32 @@ public class KarpoolController {
 		User user = repository.createUser(name, email, password, phone, rating);
 		return user.getName();
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param email
 	 * @param password
-	 * @return OK if the account is authenticated 
+	 * @return OK if the account is authenticated
 	 */
 	@GetMapping("/users/{email}")
 	public boolean authenticateUser(@PathVariable("email")String email, @PathVariable("password")String password)
 	{
 		try {
 			User user = repository.getUser(email);
-			if(user.getPassword().equals(password)) 
-				return true;	
+			if(user.getPassword().equals(password))
+				return true;
 		}
 		catch(NullPointerException e) {
 			return false;
 		}
-		
-		return false;	
+
+		return false;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param email
-	 * @return the account that was searched for 
+	 * @return the account that was searched for
 	 */
 	@GetMapping("/users/{email}")
 	public String queryUser(@PathVariable("email")String email)
@@ -81,28 +92,28 @@ public class KarpoolController {
 		Trip trip = repository.createTrip(destination,departureTime, departureLocation, seatAvailable);
 		return trip;
 	}
-	
-	
+
+
 	@RequestMapping(path="/{departureLocation}/{destination}/{seats}/")
 	public Trip queryTrip(@PathVariable("departureLocation") String departureLocation, @PathVariable("destination") String destination, @PathVariable ("seats") int seatAvailable)
 	{
 		Trip Trip = driver.getTrip();
-		
+
 		return Trip;
 	}
-	
+
 	@PostMapping("/trip/{trip}")
 	public void closeTrip(@PathVariable ("trip") Trip trip)
 	{
 		repository.closeTrip(trip);
-		
-	
+
+
 	}
 
-	
-	
+
+
 	/**
-	 * Add a  rating to the user 
+	 * Add a  rating to the user
 	 * @param email
 	 * @param rating
 	 */
@@ -112,15 +123,15 @@ public class KarpoolController {
 		try {
 			User user = repository.getUser(email);
 			user.setRating(rating);
-			
+
 		}
 		catch (NullPointerException e) {
 			System.out.println("NOT FOUND");
 		}
-				
-		
+
+
 	}
-	
+
 //	public boolean addPassenger(Passenger passenger) {
 //		boolean wasAdded = false;
 //		if (passengers.contains(passenger)) {
@@ -128,21 +139,21 @@ public class KarpoolController {
 //		}
 //		Trip existingTrip = passenger.getTrip();
 //		boolean isNewTrip = (existingTrip != null && !this.equals(existingTrip));
-//		
+//
 //		if (isNewTrip) {
 //			passenger.setTrip(this);
 //		}
-//		
+//
 //		else {
 //			passenger.add(passenger);
 //		}
 //		wasAdded = true;
 //		return wasAdded;
-//				
 //
-//		
+//
+//
 //	}
-//	
+//
 	public float Distance (int zipcode1, int zipcode2) throws MalformedURLException, IOException {
 
         BufferedReader br = null;
@@ -160,12 +171,12 @@ public class KarpoolController {
                 sb.append(line);
                 sb.append(System.lineSeparator());
             }
-            
+
             String RoughDistance = sb.toString();
             String intValue = RoughDistance.replaceAll("[^0-9, .]", "");
             float distance = Float.parseFloat(intValue);
             return distance;
-            
+
         } finally {
 
             if (br != null) {
@@ -177,4 +188,3 @@ public class KarpoolController {
 
 
 }
-
