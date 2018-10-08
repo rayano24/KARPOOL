9ac428f24c12555387e2840b4ab3b3bb539f74ca
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2018-10-07 23:09:38 -0400

    Revert "Merge branch 'master' of https://github.com/ECSE321-Fall2018/t14"
    This reverts commit 0c8a83d5663153bdef542283ebc094ac6e604134, reversing
    changes made to 48c580357e7ae12ffb7e979e07a31bebeffb39b4.

--- src/test/java/ca/mcgill/ecse321/karpool/KarpoolApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/karpool/KarpoolApplicationTests.java
@@ -13,33 +13,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Mockito.*;
 import org.mockito.invocation.InvocationOnMock;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ca.mcgill.ecse321.karpool.application.KarpoolApplication;
-<<<<<<< HEAD
 import ca.mcgill.ecse321.karpool.application.Rating;
-=======
-<<<<<<< HEAD
-import ca.mcgill.ecse321.karpool.application.Passenger;
-=======
-import ca.mcgill.ecse321.karpool.application.Rating;
->>>>>>> cc359f92f6e3fa585d382c233b4c7d07b1e4788e
->>>>>>> f8fe2efc13e2e06128a2585eaa6c0f431aa49274
 import ca.mcgill.ecse321.karpool.application.controller.KarpoolController;
 import ca.mcgill.ecse321.karpool.application.repository.KarpoolRepository;
-import ca.mcgill.ecse321.karpool.model.Trip;
 import ca.mcgill.ecse321.karpool.model.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = KarpoolApplication.class)
 public class KarpoolApplicationTests {
 	@Mock
-<<<<<<< HEAD
 	private KarpoolRepository userDao;
 
 	@InjectMocks
@@ -75,99 +63,11 @@ public class KarpoolApplicationTests {
 		});
 	}
 
-=======
-<<<<<<< HEAD
-private KarpoolRepository userDao;
-
-@InjectMocks
-private KarpoolController controller;
-
-Passenger mockPassenger = Mockito.mock(Passenger.class);
-Trip mockTrip = Mockito.mock(Trip.class);
-
-
-private static final String USER_KEY = "TestParticipant";
-private static final String NONEXISTING_KEY = "NotAParticipant";
-private static final String USER_EMAIL = "email";
-private static final String USER_PASS = "correctPass";
-private static final String USER_PASS_INCORRECT = "incorrectPass";
-private static final int NON_EXISTANT_ZIPCODE = 12345;
-private static final int zipcode1 = 90210;
-private static final int zipcode2 = 72110;
-private static final float DistanceTraveled = (float) 2341.865;
-private static final boolean wasAdded = true;
-
-
-
-@Before
-public void setMockOutput() {
-  when(userDao.getUser(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
-    if(invocation.getArgument(0).equals(USER_KEY)) {
-      User user = new User();
-      user.setName(USER_KEY);
-			user.setEmail(USER_EMAIL);
-			user.setPassword(USER_PASS);
-      return user;
-    } else {
-      return null;
-    }
- });
-}
-
-
-
-    @Test
-    public void testAddPassenger() {
-    	when(controller.addPassenger(mockPassenger, mockTrip)).thenReturn(true);
-    	assertEquals(controller.addPassenger(mockPassenger, mockTrip), wasAdded);
-    	
-    }
-=======
-	private KarpoolRepository userDao;
-
-	@InjectMocks
-	private KarpoolController controller;
-
-	private static final String USER_KEY = "TestParticipant";
-	private static final String NONEXISTING_KEY = "NotAParticipant";
-	private static final String USER_EMAIL = "email";
-	private static final String USER_PASS = "correctPass";
-	private static final String USER_PASS_INCORRECT = "incorrectPass";
-	private static final int NON_EXISTANT_ZIPCODE = 3423;
-	private static final int zipcode1 = 90210;
-	private static final int zipcode2 = 72110;
-	private static final float DistanceTraveled = (float) 2341.865;
-	private static final String USER_PHONE = "5141231234";
-	private static final String USER_PHONE_INCORRECT = "514";
-	private static final Boolean USER_NO_RECORD = false;
-	private static final Boolean USER_RECORD = true;
-	private static final Rating USER_RATING = null;
-
-	@Before
-	public void setMockOutput() {
-		when(userDao.getUser(anyString())).thenAnswer((InvocationOnMock invocation) -> {
-			if (invocation.getArgument(0).equals(USER_KEY)) {
-				User user = new User();
-				user.setName(USER_KEY);
-				user.setEmail(USER_EMAIL);
-				user.setPassword(USER_PASS);
-				return user;
-			} else {
-				return null;
-			}
-		});
-	}
-
->>>>>>> f8fe2efc13e2e06128a2585eaa6c0f431aa49274
 	// @Test
 	// public void testAddPassenger() {
 	// assertEquals(controller.addPassenger(), USER_KEY);
 	//
 	// }
-<<<<<<< HEAD
-=======
->>>>>>> cc359f92f6e3fa585d382c233b4c7d07b1e4788e
->>>>>>> f8fe2efc13e2e06128a2585eaa6c0f431aa49274
 
 	@Test
 	public void contextLoads() {
@@ -185,7 +85,6 @@ public void setMockOutput() {
 		if (test != null) {
 
 			isAccountCreatedSuccessfully = true;
-<<<<<<< HEAD
 
 		}
 		assertTrue(isAccountCreatedSuccessfully);
@@ -201,40 +100,6 @@ public void testDistanceCorrect() throws MalformedURLException, IOException {
 public void testDistanceIncorrect() throws MalformedURLException, IOException{
 	assertEquals(controller.Distance(NON_EXISTANT_ZIPCODE, zipcode2), 0,0);
 }
-=======
-
-		}
-		assertTrue(isAccountCreatedSuccessfully);
-	}
-
-	@Test
-	public void testUserQueryNotFound() {
-		assertEquals(controller.queryUser(NONEXISTING_KEY), KarpoolController.ERROR_NOT_FOUND_MESSAGE);
-	}
-
-	@Test
-	public void testAuthenticateUserPassed() {
-		assertEquals(true, controller.authenticateUser(userDao.getUser(USER_KEY).getEmail(),
-				userDao.getUser(USER_KEY).getPassword()));
-		// when(controller.authenticateUser(userDao.getUser(USER_KEY).getEmail(),
-		// userDao.getUser(USER_KEY).getPassword())).thenReturn(true);
-	}
-
-	@Test
-	public void testAuthenticateUserFailed() {
-		assertEquals(false, controller.authenticateUser(userDao.getUser(USER_KEY).getEmail(), USER_PASS_INCORRECT));
-	}
-
-	@Test
-	public void testDistanceCorrect() throws MalformedURLException, IOException {
-		assertEquals(controller.Distance(zipcode1, zipcode2), DistanceTraveled);
-	}
-
-	@Test
-	public void testDistanceIncorrect() throws MalformedURLException, IOException {
-		assertEquals(controller.Distance(NON_EXISTANT_ZIPCODE, zipcode2), 0);
-	}
->>>>>>> f8fe2efc13e2e06128a2585eaa6c0f431aa49274
 
 	@Test
 	public void testUserQueryNotFound() {
