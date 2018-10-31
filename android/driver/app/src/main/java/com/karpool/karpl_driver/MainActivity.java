e37f5fca71fe05193a7b9e1d08bebbb9ff967296
Rayan <10555572+rayano24@users.noreply.github.com>
2018-10-31 15:11:27 -0400

    Driver App (see notes)
    -Recycler view for my trips implemented
    -CreateTrip date/time pickers implemented as well as database vars
    -No settings page yet (need to fix issue on passenger app)

--- android/driver/app/src/main/java/com/karpool/karpl_driver/MainActivity.java
+++ android/driver/app/src/main/java/com/karpool/karpl_driver/MainActivity.java
@@ -17,15 +17,12 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
-
-
-
     //List that holds the fragments
     private List<Fragment> fragments = new ArrayList<>(3);
 
-    private static final String TAG_FRAGMENT_HOME = "tag_frag_home";
-    private static final String TAG_FRAGMENT_DASHBOARD = "tag_frag_dashboard";
-    private static final String TAG_FRAGMENT_NOTIFICATIONS = "tag_frag_notifications";
+    private static final String TAG_FRAGMENT_CREATE = "tag_frag_create";
+    private static final String TAG_FRAGMENT_TRIPS = "tag_frag_trips";
+    private static final String TAG_FRAGMENT_SETTINGS = "tag_frag_settings";
 
     private List<String> participantNames = new ArrayList<>();
     private ArrayAdapter<String> participantAdapter;
@@ -40,14 +37,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    switchFragment(0, TAG_FRAGMENT_HOME);
+                case R.id.navigation_create:
+                    switchFragment(0, TAG_FRAGMENT_CREATE);
                     return true;
-                case R.id.navigation_dashboard:
-                    switchFragment(1, TAG_FRAGMENT_DASHBOARD);
+                case R.id.navigation_trips:
+                    switchFragment(1, TAG_FRAGMENT_TRIPS);
                     return true;
-                case R.id.navigation_notifications:
-                    switchFragment(2, TAG_FRAGMENT_NOTIFICATIONS);
+                case R.id.navigation_settings:
+                    switchFragment(2, TAG_FRAGMENT_SETTINGS);
                     return true;
             }
             return false;
@@ -65,9 +62,8 @@ public class MainActivity extends AppCompatActivity {
         buildFragmentsList();
 
 
-        switchFragment(0, TAG_FRAGMENT_HOME);
+        switchFragment(0, TAG_FRAGMENT_CREATE);
 
-        refreshErrorMessage(); //keep this at the end
 
 
 
@@ -83,9 +79,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void buildFragmentsList() {
-        FragmentOne homeFragment = buildFragmentOne("Home");
-        FragmentTwo dashboardFragment = buildFragmentTwo("Dashboard");
-        FragmentThree notificationsFragment = buildFragmentThree("Notifications");
+        FragmentOne homeFragment = buildFragmentOne("Create");
+        FragmentTwo dashboardFragment = buildFragmentTwo("My Trips");
+        FragmentThree notificationsFragment = buildFragmentThree("Settings");
 
         fragments.add(homeFragment);
         fragments.add(dashboardFragment);
@@ -113,86 +109,6 @@ public class MainActivity extends AppCompatActivity {
         Bundle bundle = new Bundle();
         fragment.setArguments(bundle);
         return fragment;
-    }
-
-    private void refreshErrorMessage() {
-        // set the error message
-        TextView tvError = (TextView) findViewById(0); //dont leave this as 0 pls pls
-        tvError.setText(error);
-
-        if (error == null || error.length() == 0) {
-            tvError.setVisibility(View.GONE);
-        } else {
-            tvError.setVisibility(View.VISIBLE);
-        }
-
-    }
-
-    private Bundle getTimeFromLabel(String text) {
-        Bundle rtn = new Bundle();
-        String comps[] = text.toString().split(":");
-        int hour = 12;
-        int minute = 0;
-
-        if (comps.length == 2) {
-            hour = Integer.parseInt(comps[0]);
-            minute = Integer.parseInt(comps[1]);
-        }
-
-        rtn.putInt("hour", hour);
-        rtn.putInt("minute", minute);
-
-        return rtn;
-    }
-
-    private Bundle getDateFromLabel(String text) {
-        Bundle rtn = new Bundle();
-        String comps[] = text.toString().split("-");
-        int day = 1;
-        int month = 1;
-        int year = 1;
-
-        if (comps.length == 3) {
-            day = Integer.parseInt(comps[0]);
-            month = Integer.parseInt(comps[1]);
-            year = Integer.parseInt(comps[2]);
-        }
-
-        rtn.putInt("day", day);
-        rtn.putInt("month", month-1);
-        rtn.putInt("year", year);
-
-        return rtn;
-    }
-
-    public void showTimePickerDialog(View v) {
-        TextView tf = (TextView) v;
-        Bundle args = getTimeFromLabel(tf.getText().toString());
-        args.putInt("id", v.getId());
-
-        TimePickerFragment newFragment = new TimePickerFragment();
-        newFragment.setArguments(args);
-        newFragment.show(getSupportFragmentManager(), "timePicker");
-    }
-
-    public void showDatePickerDialog(View v) {
-        TextView tf = (TextView) v;
-        Bundle args = getDateFromLabel(tf.getText().toString());
-        args.putInt("id", v.getId());
-
-        DatePickerFragment newFragment = new DatePickerFragment();
-        newFragment.setArguments(args);
-        newFragment.show(getSupportFragmentManager(), "datePicker");
-    }
-
-    public void setTime(int id, int h, int m) {
-        TextView tv = (TextView) findViewById(id);
-        tv.setText(String.format("%02d:%02d", h, m));
-    }
-
-    public void setDate(int id, int d, int m, int y) {
-        TextView tv = (TextView) findViewById(id);
-        tv.setText(String.format("%02d-%02d-%04d", d, m + 1, y));
     }
 
 
---
+++ android/driver/app/src/main/java/com/karpool/karpl_driver/tripAdapter.java
@@ -0,0 +1,56 @@
+package com.karpool.karpl_driver;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class tripAdapter extends RecyclerView.Adapter<tripAdapter.MyViewHolder> {
+
+    private List<Trip> tripList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        protected TextView origin, destination, date, time;
+
+        public MyViewHolder(View view) {
+            super(view);
+            origin = (TextView) view.findViewById(R.id.origin);
+            destination = (TextView) view.findViewById(R.id.destination);
+            date = (TextView) view.findViewById(R.id.date);
+            time = (TextView) view.findViewById(R.id.time);
+
+        }
+    }
+
+
+    public tripAdapter(List<Trip> tripList) {
+        this.tripList = tripList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_trip, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Trip trip = tripList.get(position);
+        holder.origin.setText(trip.getOrigin());
+        holder.destination.setText(trip.getDestination());
+        holder.date.setText(trip.getDate());
+        holder.time.setText(trip.getTime());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return tripList.size();
+    }
+}
