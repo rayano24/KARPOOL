4a2a57128f2a697d85849662eea7a26ece055f6f
larakollokian <lara.kollokian@gmail.com>
2018-10-31 17:23:20 -0400

    fixed minor issue with table ID conflict by deleting and regenerating all the tables

--- src/main/java/ca/mcgill/ecse321/karpool/application/model/Driver.java
+++ src/main/java/ca/mcgill/ecse321/karpool/application/model/Driver.java
@@ -1,8 +1,6 @@
 package ca.mcgill.ecse321.karpool.application.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
