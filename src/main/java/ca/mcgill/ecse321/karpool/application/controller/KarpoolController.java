9febd3d530c1f5495fc8c7636e165b693fc8ec70
Omar Yunus <omar.hussain@mail.mcgill.ca>
2018-11-19 13:58:38 -0500

    Cross origin change

--- src/main/java/ca/mcgill/ecse321/karpool/application/controller/KarpoolController.java
+++ src/main/java/ca/mcgill/ecse321/karpool/application/controller/KarpoolController.java
@@ -7,6 +7,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.karpool.application.repository.*;
-
+@CrossOrigin
 @RestController
 public class KarpoolController {
 
